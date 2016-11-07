36a37fb8d6972827773b6298002897d64155167f
zenranda <XanderJones@hushmail.com>
2016-11-07 02:10:30 -0800

    fixed tests

--- test_main.py
+++ test_main.py
@@ -3,6 +3,8 @@
 ###
 import flask_main
 import arrow
+import datetime
+from dateutil import tz
 
 # Mongo database
 from pymongo import MongoClient
@@ -26,21 +28,21 @@ except:
 
     
 def test_dates():
-    assert(flask_main.humanize_arrow_date(arrow.utcnow()) == "Today") 
-    assert(flask_main.humanize_arrow_date(arrow.utcnow().replace(days=+7)) == "in 7 days")
-    assert(flask_main.humanize_arrow_date(arrow.utcnow().replace(years=+7)) == "in a year")
-    assert(flask_main.humanize_arrow_date(arrow.utcnow().replace(days=-1, hours=+4, minutes=+49)) == 'Yesterday')
+    assert(flask_main.humanize_arrow_date(arrow.utcnow().naive) == "Today") 
+    assert(flask_main.humanize_arrow_date(arrow.utcnow().replace(days=+7).naive) == "in 7 days")
+    assert(flask_main.humanize_arrow_date(arrow.utcnow().replace(years=+1).naive) == "in a year")
+    assert(flask_main.humanize_arrow_date(arrow.utcnow().replace(days=-1, hours=+4, minutes=+49).naive) == 'Yesterday')
 
 def test_insertion_and_removal():
-    flask_main.add_memo(collection, {"type" : "dated_memo", "date" : arrow.get(utcnow()), "text" : "The first entry." })
-    flask_main.add_memo(collection, {"type" : "dated_memo", "date" : arrow.get(utcnow().replace(days=+19), "text" : "The second entry." })
-    flask_main.add_memo(collection, {"type" : "dated_memo", "date" : arrow.get(utcnow().replace(days=+1, minutes=+9), "text" : "The third and final entry." })
+    flask_main.add_memo(collection, {"type" : "dated_memo", "date" : arrow.utcnow().naive, "text" : "The first entry." })
+    flask_main.add_memo(collection, {"type" : "dated_memo", "date" : arrow.utcnow().replace(days=+19).naive, "text" : "The second entry." })
+    flask_main.add_memo(collection, {"type" : "dated_memo", "date" : arrow.utcnow().replace(days=+1, minutes=+9).naive, "text" : "The third and final entry." })
     assert(collection.count() == (base_size+3))
     flask_main.remove_memo(collection, "The second entry.")
     assert(collection.count() == (base_size+2))
     flask_main.remove_memo(collection, "The first entry.")
     assert(collection.count() == (base_size+1))
-    flask_main.add_memo(collection, {"type" : "dated_memo", "date" : arrow.get(utcnow().replace(days=+100, minutes=+2), "text" : "The fourth and seriously final entry." })
+    flask_main.add_memo(collection, {"type" : "dated_memo", "date" : arrow.utcnow().replace(days=+100, minutes=+2).naive, "text" : "The fourth and seriously final entry." })
     assert(collection.count() == (base_size+2))
     flask_main.remove_memo(collection, "The third and final entry.")
     flask_main.remove_memo(collection, "The fourth and seriously final entry.")
