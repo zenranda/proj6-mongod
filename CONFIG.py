6f33133aa1d518e079318806e10a2431e4f0a324
zenranda <XanderJones@hushmail.com>
2016-11-06 23:16:12 -0800

    rev

---
+++ CONFIG.py
@@ -0,0 +1,11 @@
+"""
+Configuration of memo app. 
+Generated Sat  5 Nov 04:56:41 UTC 2016
+Edit to fit development or deployment environment.
+
+"""
+
+PORT=5000
+DEBUG = True  # Set to False for production use
+secret_key="a20dd5098d312988b8794e6cce000ee9"
+
